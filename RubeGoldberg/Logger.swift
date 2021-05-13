aa11cd35c12810015c01a53e2c63d1d2965f2f12
Kiril <ikera008@gmail.com>
2021-05-13 15:29:41 +0300

    Initial Commit

---
+++ RubeGoldberg/Logger.swift
@@ -0,0 +1,31 @@
+//
+//  Logger.swift
+//  RubeGoldberg
+//
+//  Created by Кирилл Рязанцев on 13.05.2021.
+//
+
+import Foundation
+
+protocol LoggerProtocol{
+    var message: String { get set };
+    mutating func printMessage() -> Void
+}
+
+struct Logger: LoggerProtocol{
+    
+    var message: String = ""
+    
+    mutating func printMessage() -> Void {
+        if self.message.isEmpty {
+            print("Nothing to print")
+        }else{
+            print(self.message)
+        }
+    }
+
+    
+    init(_ message: String){
+        self.message = message;
+    }
+}
---
+++ RubeGoldberg/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RubeGoldberg
+//
+//  Created by Кирилл Рязанцев on 13.05.2021.
+//
+
+import Foundation
+
+var message = Logger("Hello world");
+message.printMessage();
+
